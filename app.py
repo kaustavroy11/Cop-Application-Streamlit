b146bff35ebd3798c18efcc6a5cda78447191628
kaustavroy11 <kaustav.roy.960@gmail.com>
2024-04-08 23:42:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import helper
+
+
+st.sidebar.title("Cop Application")
+# Load your data
+uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    
+    
+    if st.sidebar.button("Show Analysis"):
+    # Read the uploaded file into a pandas DataFrame
+        
