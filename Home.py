24007b7bed068d12149c3146181d5e6e7f3bd27c
kaustavroy11 <kaustav.roy.960@gmail.com>
2024-04-24 23:59:09 +0530

    commit

--- Home.py
+++ Home.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
+# Set the page configuration
 st.set_page_config(
     page_title="-Xtra Help",
 )
 
+# Display the title and welcome message
 st.markdown("<h1 style='text-align: center;'>-Xtra Help</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>WELCOMES YOU!</h2>", unsafe_allow_html=True)
-st.header("Safely Guard people and get instant help with proper Information")
-st.markdown("A crime detection helps in tracking to alert users about criminal activity in their vicinity, enhancing personal security and fostering safer communities through prompt intervention and awareness.")
+
+# Add a header and description
+st.markdown("<h3>Safely Guard people and get instant help with proper information</h3>", unsafe_allow_html=True)
+st.markdown("<h3>Here we have given you four options to analysis your dataset in different ways</h3>", unsafe_allow_html=True)
+st.markdown("<h4>To Enter your Dataset Click on the options present on ther sidebar and Click on Show Analiysis</h4>",unsafe_allow_html=True)
